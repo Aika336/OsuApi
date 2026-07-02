db57b765bce5f3039fe9df192863e51318422c8a
Aika336 <vladock1999Suka@gmail.com>
2026-07-02 23:23:25 +0200

    feat: add base function of class

--- include/MemoryScanning/PatternMatcher.h
+++ include/MemoryScanning/PatternMatcher.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <vector>
+#include <string>
+
+class PatternMatcher {
+	std::vector<uint8_t> signature;
+	std::vector<uint8_t> mask;
+public:
+	PatternMatcher() = delete;
+	PatternMatcher(const std::vector<uint8_t>& signature, const std::vector<uint8_t>& mask) : signature(signature), mask(mask) {}
+	int CheckForStableSignature(const std::vector<uint8_t>& region_bytes) const;
+};

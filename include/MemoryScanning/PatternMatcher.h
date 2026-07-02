7e55131a60966abd4ab2807d412e7bacd73a5877
Aika336 <vladock1999Suka@gmail.com>
2026-07-02 23:55:55 +0200

    feat: implement PatternMatcher

--- include/MemoryScanning/PatternMatcher.h
+++ include/MemoryScanning/PatternMatcher.h
@@ -2,12 +2,19 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 
 class PatternMatcher {
 	std::vector<uint8_t> signature;
 	std::vector<uint8_t> mask;
+	int offset;
 public:
 	PatternMatcher() = delete;
-	PatternMatcher(const std::vector<uint8_t>& signature, const std::vector<uint8_t>& mask) : signature(signature), mask(mask) {}
+	PatternMatcher(const std::vector<uint8_t>& signature, const std::vector<uint8_t>& mask, int offset) : signature(signature), mask(mask), offset(offset) {
+		if (signature.size() != mask.size()) {
+			std::cerr << "Error: Signature and mask must be of the same size." << std::endl;
+		}
+	}
 	int CheckForStableSignature(const std::vector<uint8_t>& region_bytes) const;
+	int getOffset() const { return offset; }
 };

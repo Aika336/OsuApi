33dd06a59b7f90adb290e4867ccce7eb5837ef07
Aika336 <vladock1999Suka@gmail.com>
2026-07-02 13:36:53 +0200

    fix: move constructor fix

--- include/SignatureFinder/HandlerRaii.h
+++ include/SignatureFinder/HandlerRaii.h
@@ -3,10 +3,32 @@
 #include <Windows.h>
 
 struct Handler_raii {
-    HANDLE hProcess;
+    HANDLE hProcess = NULL;
+
+    Handler_raii() = default;
+    Handler_raii(const Handler_raii&) = delete;
+    Handler_raii& operator=(const Handler_raii&) = delete;
+
+	Handler_raii(Handler_raii&& other) noexcept : hProcess(other.hProcess) {
+		other.hProcess = NULL;
+	}
+
+	Handler_raii& operator=(Handler_raii&& other) noexcept {
+		if (this != &other) {
+			if (hProcess != NULL && hProcess != INVALID_HANDLE_VALUE) {
+				CloseHandle(hProcess);
+				hProcess = NULL;
+			}
+			hProcess = other.hProcess;
+			other.hProcess = NULL;
+		}
+		return *this;
+	}
+
     ~Handler_raii() {
-        if (hProcess != NULL || hProcess == INVALID_HANDLE_VALUE) {
+        if (hProcess != NULL && hProcess != INVALID_HANDLE_VALUE) {
             CloseHandle(hProcess);
+            hProcess = NULL;
         }
     }
 };

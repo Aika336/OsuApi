e493b12497b9e0465b6e4c32027c9145ae708c98
Aika336 <vladock1999Suka@gmail.com>
2026-07-02 14:34:47 +0200

    feat: add RPM(ReadProcessMemory) function

---
+++ include/Memory/Memory.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../SignatureFinder/HandlerRaii.h"
+#include <Windows.h>
+#include <string>
+#include <vector>
+
+class Memory {
+public:
+	template<typename T>
+	static T RPM(Handler_raii handler, uintptr_t address);
+
+	static std::wstring ReadDotNetString(HANDLE hProcess, uintptr_t string_address);
+	static std::vector<uint8_t> ReadMemoryRegion(HANDLE hProcess, uintptr_t address, size_t size);
+};
+
+template<typename T>
+inline T Memory::RPM(Handler_raii handler, uintptr_t address)
+{
+	T value{};
+	SIZE_T read_bytes = 0;
+
+	if (!ReadProcessMemory(handler.hProcess, static_cast<LPVOID>(address), &value, sizeof(T), &read_bytes) 
+		|| read_bytes != sizeof(T)) {
+		std::cout << "Failed to read memory at address: " << std::hex << address << std::dec << std::endl;
+		return T{};
+	}
+
+	return value;
+}

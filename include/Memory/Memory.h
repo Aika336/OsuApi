bc54f8273f9482c9fca6726b677902aaf6ac662b
Aika336 <vladock1999Suka@gmail.com>
2026-07-02 14:43:11 +0200

    add print error if memory region bad scanded

--- include/Memory/Memory.h
+++ include/Memory/Memory.h
@@ -4,6 +4,7 @@
 #include <Windows.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 class Memory {
 public:
@@ -33,6 +34,7 @@ inline std::vector<uint8_t> Memory::ReadMemoryRegion(Handler_raii handler, uintp
 	std::vector<uint8_t> buffer(size);
 	SIZE_T read_bytes = 0;
 	if (!ReadProcessMemory(handler.hProcess, reinterpret_cast<LPVOID>(address), buffer.data(), size, &read_bytes)) {
+		std::cout << "Failed to read memory region at address: " << std::hex << address << std::dec << std::endl;
 		buffer.clear();
 	}
 

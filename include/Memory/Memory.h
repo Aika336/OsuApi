5d232d2d644db377d7e5343db6e21f7131c43e30
Aika336 <vladock1999Suka@gmail.com>
2026-07-02 15:04:05 +0200

    feat: fix problem with CMake

--- include/Memory/Memory.h
+++ include/Memory/Memory.h
@@ -28,14 +28,3 @@ inline T Memory::RPM(Handler_raii handler, uintptr_t address)
 
 	return value;
 }
-
-inline std::vector<uint8_t> Memory::ReadMemoryRegion(Handler_raii handler, uintptr_t address, size_t size) {
-	std::vector<uint8_t> buffer(size);
-	SIZE_T read_bytes = 0;
-	if (!ReadProcessMemory(handler.hProcess, reinterpret_cast<LPVOID>(address), buffer.data(), size, &read_bytes)) {
-		std::cout << "Failed to read memory region at address: " << std::hex << address << std::dec << std::endl;
-		buffer.clear();
-	}
-
-	return buffer;
-}
---
+++ src/Memory/Memory.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Memory.h"
+
+std::vector<uint8_t> Memory::ReadMemoryRegion(Handler_raii handler, uintptr_t address, size_t size) {
+	std::vector<uint8_t> buffer(size);
+	SIZE_T read_bytes = 0;
+	if (!ReadProcessMemory(handler.hProcess, reinterpret_cast<LPVOID>(address), buffer.data(), size, &read_bytes)) {
+		std::cout << "Failed to read memory region at address: " << std::hex << address << std::dec << std::endl;
+		buffer.clear();
+	}
+
+	return buffer;
+}

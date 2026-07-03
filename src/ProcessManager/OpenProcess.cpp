c00abe0187622d8c4d2eeb7b4c418cc1ed91b88e
Aika336 <vladock1999Suka@gmail.com>
2026-07-04 00:32:27 +0200

    back: what was a big mistake

--- src/ProcessManager/OpenProcess.cpp
+++ src/ProcessManager/OpenProcess.cpp
@@ -2,41 +2,41 @@
 
 #include <iostream>
 
-std::shared_ptr<Handler_raii> OpenProcess::OpenProcessByIdW(ProcessInfo info)
+Handler_raii OpenProcess::OpenProcessByIdW(ProcessInfo info)
 {
 	HANDLE hProcess = ::OpenProcess(PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION, FALSE, info.processId);
 	if (hProcess == NULL) {
 		std::cout << "OpenProcessByIdW: Failed to open process with ID " << info.processId << ". Error: " << GetLastError() << std::endl;
 		return {};
 	}
-	std::shared_ptr<Handler_raii> handler = std::make_shared<Handler_raii>();
-	handler->hProcess = hProcess;
+	Handler_raii handler;
+	handler.hProcess = hProcess;
 
 	return handler;
 }
 
-std::shared_ptr<Handler_raii> OpenProcess::OpenProcessByIdR(ProcessInfo info)
+Handler_raii OpenProcess::OpenProcessByIdR(ProcessInfo info)
 {
 	HANDLE hProcess = ::OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, info.processId);
 	if (hProcess == NULL) {
 		std::cout << "OpenProcessByIdR: Failed to open process with ID " << info.processId << ". Error: " << GetLastError() << std::endl;
 		return {};
 	}
-	std::shared_ptr<Handler_raii> handler = std::make_shared<Handler_raii>();
-	handler->hProcess = hProcess;
+	Handler_raii handler;
+	handler.hProcess = hProcess;
 
 	return handler;
 }
 
-std::shared_ptr<Handler_raii> OpenProcess::OpenProcessByIdRW(ProcessInfo info)
+Handler_raii OpenProcess::OpenProcessByIdRW(ProcessInfo info)
 {
 	HANDLE hProcess = ::OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION, FALSE, info.processId);
 	if (hProcess == NULL) {
 		std::cout << "OpenProcessByIdRW: Failed to open process with ID " << info.processId << ". Error: " << GetLastError() << std::endl;
 		return {};
 	}
-	std::shared_ptr<Handler_raii> handler = std::make_shared<Handler_raii>();
-	handler->hProcess = hProcess;
+	Handler_raii handler;
+	handler.hProcess = hProcess;
 
 	return handler;
 }

6cded827597295aaf933edd38fea3ec0b63d230c
Aika336 <vladock1999Suka@gmail.com>
2026-07-03 18:43:37 +0200

    bruh

--- include/ProvideOsuInfo/ProviderOsuInfo.h
+++ include/ProvideOsuInfo/ProviderOsuInfo.h
@@ -9,7 +9,6 @@ class ProvideOsuInfo {
 	const Handler_raii& handler_;
 
 	std::optional<uintptr_t> GetCurrentScreenAddress();
-	std::optional<uintptr_t> GetCurrentScreenAddress();
 public:
 	ProvideOsuInfo(const Handler_raii& handler, const uintptr_t &base_address) : base_address_(base_address), handler_(handler){}
 	std::optional<uint32_t> GetCurrentCombo();

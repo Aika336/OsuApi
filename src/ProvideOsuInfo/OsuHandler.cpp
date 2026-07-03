924b81d0ee7e07af37c57a1015ac07d7c2de72a1
Aika336 <vladock1999Suka@gmail.com>
2026-07-03 20:04:22 +0200

    feat: implements default construction

---
+++ src/ProvideOsuInfo/OsuHandler.cpp
@@ -0,0 +1,21 @@
+#include "OsuHandler.h"
+#include <OpenProcess.h>
+#include "../../include/MemoryScanning/ProcessMemoryScanner.h"
+#include <ProvideProcesses.h>
+
+OsuHandler::OsuHandler()
+{
+	auto process_info = ProvideProcesses::GetProcessByName(L"osu!.exe");
+	if (!process_info) {
+		std::cout << "Process not found." << std::endl;
+	}
+
+	Handler_raii handler = OpenProcess::OpenProcessByIdRW(process_info.value());
+
+	auto game_base_address = ProcessMemoryScanner::ScanProcessMemoryForSignature(handler, PatternMatcher(signature_, mask_, 32));
+	if (!game_base_address) {
+		std::cout << "Failed to find the game base address." << std::endl;
+	}
+
+	osu_info_ = new ProvideOsuInfo(handler, game_base_address.value());
+}

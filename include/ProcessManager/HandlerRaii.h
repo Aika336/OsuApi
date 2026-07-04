1d1d33b55abc42c04b1bca60eaef814ebb84ab60
Aika336 <vladock1999Suka@gmail.com>
2026-07-04 10:34:28 +0200

    refactor: refactor handler_raii for normal uses

--- include/ProcessManager/HandlerRaii.h
+++ include/ProcessManager/HandlerRaii.h
@@ -6,26 +6,30 @@ struct Handler_raii {
     HANDLE hProcess = NULL;
 
     Handler_raii() = default;
+    Handler_raii(HANDLE h) : hProcess(h) {}
+
     Handler_raii(const Handler_raii&) = delete;
     Handler_raii& operator=(const Handler_raii&) = delete;
 
-	Handler_raii(Handler_raii&& other) noexcept : hProcess(other.hProcess) {
-		other.hProcess = NULL;
-	}
+    Handler_raii(Handler_raii&& other) noexcept : hProcess(other.hProcess) {
+        other.hProcess = NULL;
+    }
 
-	Handler_raii& operator=(Handler_raii&& other) noexcept {
-		if (this != &other) {
-			if (hProcess != NULL && hProcess != INVALID_HANDLE_VALUE) {
-				CloseHandle(hProcess);
-				hProcess = NULL;
-			}
-			hProcess = other.hProcess;
-			other.hProcess = NULL;
-		}
-		return *this;
-	}
+    Handler_raii& operator=(Handler_raii&& other) noexcept {
+        if (this != &other) {
+            Close();
+            hProcess = other.hProcess;
+            other.hProcess = NULL;
+        }
+        return *this;
+    }
 
     ~Handler_raii() {
+        Close();
+    }
+
+private:
+    void Close() {
         if (hProcess != NULL && hProcess != INVALID_HANDLE_VALUE) {
             CloseHandle(hProcess);
             hProcess = NULL;

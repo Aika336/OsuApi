b3db388cdfa82f3f65ebf073a2f3a5c79b030152
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 17:39:09 +0200

    feat: rewrote the function for getting all processes via ntdll

--- src/ProcessManager/ProcessProvider.cpp
+++ src/ProcessManager/ProcessProvider.cpp
@@ -1,5 +1,6 @@
 #include "ProcessProvider.h"
 #include "Logger.h"
+#include "Memory.h"
 
 #include <TlHelp32.h>
 #include <iostream>
@@ -20,7 +21,7 @@ std::optional<ProcessInfo> ProcessProvider::FindProcessByName(const std::wstring
 	throw std::runtime_error("ProvideProcesses::FindProcessByName: No find process by name " + std::string(name.begin(), name.end()));
 }
 
-std::vector<ProcessInfo> ProcessProvider::GetAllProcesses() {
+/*std::vector<ProcessInfo> ProcessProvider::GetAllProcesses() {
 	std::vector<ProcessInfo> processes;
 	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	if (snapshot == INVALID_HANDLE_VALUE) {
@@ -36,6 +37,55 @@ std::vector<ProcessInfo> ProcessProvider::GetAllProcesses() {
 	}
 
 	CloseHandle(snapshot);
+	return processes;
+}*/
+
+std::vector<ProcessInfo> ProcessProvider::GetAllProcesses() {
+	std::vector<ProcessInfo> processes;
+	
+	ULONG systeminfo_len = 0;
+	NTSTATUS status = Memory::ntdll_.NtQuerySystemInformation(
+		SystemProcessInformation,
+		nullptr,
+		systeminfo_len,
+		&systeminfo_len
+	);
+
+	const NTSTATUS STATUS_INFO_LENGTH_MISMATCH = 0xC0000004;
+	if (status != STATUS_INFO_LENGTH_MISMATCH) {
+		LOG_ERROR("Can't get sysinfo's length");
+		throw std::runtime_error("Can't get sysinfo's length");
+	}
+
+	std::vector<BYTE> buffer(systeminfo_len);
+	status = Memory::ntdll_.NtQuerySystemInformation(
+		SystemProcessInformation,
+		buffer.data(),
+		systeminfo_len,
+		&systeminfo_len
+	);
+
+	if (!NT_SUCCESS(status)) {
+		LOG_ERROR("Can't get system information");
+		throw std::runtime_error("Can't get system information");
+	}
+
+	PSYSTEM_PROCESS_INFORMATION sysinfo =
+		reinterpret_cast<PSYSTEM_PROCESS_INFORMATION>(buffer.data());
+
+	while (true) {
+		processes.emplace_back(
+			HandleToLong(sysinfo->UniqueProcessId), 
+			UnicodeStringToWString(sysinfo->ImageName));
+
+		if (sysinfo->NextEntryOffset == 0) {
+			break;
+		}
+		sysinfo = reinterpret_cast<PSYSTEM_PROCESS_INFORMATION>(
+			reinterpret_cast<BYTE*>(sysinfo) + sysinfo->NextEntryOffset
+			);
+	}
+
 	return processes;
 }
 

064c4916a1b355423e675ce8a014a8e37838b243
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 16:27:40 +0200

    refactor: ReadMemoryRegion rewritten using ntdll

--- src/Memory/Memory.cpp
+++ src/Memory/Memory.cpp
@@ -1,18 +1,32 @@
 #include "Memory.h"
 #include "Logger.h"
 
-#include <Windows.h>
 #include <vector>
 
+NtDll Memory::ntdll_;
+
 std::optional<std::vector<uint8_t>> Memory::ReadMemoryRegion(
-	const HandleRaii& handler, 
+	const HandleRaii& handle, 
 	uintptr_t address, 
 	size_t size) 
 {
 	std::vector<uint8_t> buffer(size);
 	SIZE_T read_bytes = 0;
-	if (!ReadProcessMemory(handler.hProcess, reinterpret_cast<LPVOID>(address), buffer.data(), size, &read_bytes)) {
+
+	NTSTATUS status = ntdll_.NtReadProcessMemory(
+		handle.hProcess,
+		reinterpret_cast<LPVOID>(address),
+		buffer.data(),
+		size,
+		&read_bytes
+	);
+
+	if (!NT_SUCCESS(status)) {
 		LOG_ERROR("Failed to read memory region at address: " + std::format("0x{:x}", address));
+		return std::nullopt;
+	}
+	else if (buffer.size() != read_bytes) {
+		LOG_ERROR("Read bytes != expected bytes: " + std::format("0x{:x}", address));
 		return std::nullopt;
 	}
 

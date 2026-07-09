67dfb8d4f473fcc40da5033b86c8a7cde6572df8
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 18:07:18 +0200

    refactor: Process openers now uses ntdll

--- src/ProcessManager/ProcessOpener.cpp
+++ src/ProcessManager/ProcessOpener.cpp
@@ -1,41 +1,91 @@
 #include "ProcessOpener.h"
 #include "Logger.h"
+#include "Memory.h"
 
 #include <iostream>
 #include <string>
+#include <format>
 
 HandleRaii ProcessOpener::OpenProcessForWrite(ProcessInfo info)
 {
-	HandleRaii hProcess {::OpenProcess(PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION, FALSE, info.processId)};
-	if (!hProcess.hProcess) {
-		std::string msg = "OpenProcessForWrite: Failed to open process with ID " + info.processId;
-		msg += ". Error: " + GetLastError();
+	CLIENT_ID client_id{ reinterpret_cast<HANDLE>(static_cast<ULONG_PTR>(info.processId)), nullptr };
+	OBJECT_ATTRIBUTES attributes = { sizeof(attributes) };
+	HandleRaii handle{0};
+
+	NTSTATUS status = Memory::ntdll_.NtOpenProcess(
+		&(handle.hProcess),
+		PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION,
+		&attributes,
+		&client_id
+	);
+
+	if (!NT_SUCCESS(status)) {
+		std::string msg = "NtOpenProcess failed for PID " + std::to_string(info.processId);
+		msg += " with status: " + std::format("0x{:x}", status);
 		LOG_ERROR(msg);
 		throw std::runtime_error(msg);
 	}
-	return hProcess;
+	else if (!handle.hProcess || handle.hProcess == INVALID_HANDLE_VALUE) {
+		std::string msg = "Error open process #" + std::string(info.name.begin(), info.name.end());
+		LOG_ERROR(msg);
+		throw std::runtime_error(msg);
+	}
+
+	return handle;
 }
 
 HandleRaii ProcessOpener::OpenProcessForRead(ProcessInfo info)
 {
-	HandleRaii hProcess = { ::OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, info.processId) };
-	if (!hProcess.hProcess) {
-		std::string msg = "OpenProcessForRead: Failed to open process with ID " + info.processId;
-		msg += ". Error: " + GetLastError();
+	CLIENT_ID client_id{ reinterpret_cast<HANDLE>(static_cast<ULONG_PTR>(info.processId)), nullptr };
+	OBJECT_ATTRIBUTES attributes = { sizeof(attributes) };
+	HandleRaii handle{ 0 };
+
+	NTSTATUS status = Memory::ntdll_.NtOpenProcess(
+		&(handle.hProcess),
+		PROCESS_VM_READ | PROCESS_QUERY_INFORMATION,
+		&attributes,
+		&client_id
+	);
+
+	if (!NT_SUCCESS(status)) {
+		std::string msg = "NtOpenProcess failed for PID " + std::to_string(info.processId);
+		msg += " with status: " + std::format("0x{:x}", status);
 		LOG_ERROR(msg);
 		throw std::runtime_error(msg);
 	}
-	return hProcess;
+	else if (!handle.hProcess || handle.hProcess == INVALID_HANDLE_VALUE) {
+		std::string msg = "Error open process #" + std::string(info.name.begin(), info.name.end());
+		LOG_ERROR(msg);
+		throw std::runtime_error(msg);
+	}
+
+	return handle;
 }
 
 HandleRaii ProcessOpener::OpenProcessForReadWrite(ProcessInfo info)
 {
-	HandleRaii hProcess = { ::OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, info.processId) };
-	if (!hProcess.hProcess) {
-		std::string msg = "OpenProcessForReadWrite: Failed to open process with ID " + info.processId;
-		msg += ". Error: " + GetLastError();
+	CLIENT_ID client_id{ reinterpret_cast<HANDLE>(static_cast<ULONG_PTR>(info.processId)), nullptr };
+	OBJECT_ATTRIBUTES attributes = { sizeof(attributes) };
+	HandleRaii handle{ 0 };
+
+	NTSTATUS status = Memory::ntdll_.NtOpenProcess(
+		&(handle.hProcess),
+		PROCESS_VM_WRITE | PROCESS_VM_READ | PROCESS_QUERY_INFORMATION,
+		&attributes,
+		&client_id
+	);
+
+	if (!NT_SUCCESS(status)) {
+		std::string msg = "NtOpenProcess failed for PID " + std::to_string(info.processId);
+		msg += " with status: " + std::format("0x{:x}", status);
 		LOG_ERROR(msg);
 		throw std::runtime_error(msg);
 	}
-	return hProcess;
+	else if (!handle.hProcess || handle.hProcess == INVALID_HANDLE_VALUE) {
+		std::string msg = "Error open process #" + std::string(info.name.begin(), info.name.end());
+		LOG_ERROR(msg);
+		throw std::runtime_error(msg);
+	}
+
+	return handle;
 }

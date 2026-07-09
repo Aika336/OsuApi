e39adc5f8799468dc36d02a412e1332c67f55df8
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 14:35:16 +0200

    refactor: change namespace name

--- NtDll/include/domain.h
+++ NtDll/include/domain.h
@@ -11,7 +11,7 @@
 #include <bcrypt.h>
 #include <stdexcept>
 
-namespace ntfunctions_name {
+namespace nt_functions_name {
     static constexpr std::string_view NTDLL = "ntdll.dll";
     static constexpr std::string_view ADJUST_PRIVILEGE = "NtAdjustPrivilegesToken";
     static constexpr std::string_view NTQSI = "NtQuerySystemInformation";

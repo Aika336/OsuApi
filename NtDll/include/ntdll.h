e6d8f278d7d4820434d7ab1191f7148da269a4b9
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 14:18:55 +0200

    feat: adding declarations of pointers for ntdll functions

---
+++ NtDll/include/ntdll.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "domain.h"
+
+#include <Windows.h>
+#include <winternl.h>
+
+class NtDll {
+    HMODULE ntdll_{ 0 };
+
+
+	void LoadNtQuerySystemInformation();
+	void LoadNtOpenProcess();
+	void LoadNtReadProcessMemory();
+public:
+    NTSTATUS NtQuerySystemInformation(
+        SYSTEM_INFORMATION_CLASS SystemInformationClass,
+        PVOID SystemInformation,
+        ULONG SystemInformationLength,
+        PULONG ReturnLength
+    );
+    NTSTATUS NtOpenProcess(
+        PHANDLE hProcess,
+        ACCESS_MASK desired_access,
+        POBJECT_ATTRIBUTES object_attributes,
+        CLIENT_ID* client_id
+    );
+    NTSTATUS NtReadProcessMemory(
+        HANDLE ProcessHandle,
+        PVOID BaseAddress,
+        PVOID Buffer,
+        SIZE_T NumberOfBytesToRead,
+        PSIZE_T NumberOfBytesRead
+    );
+};

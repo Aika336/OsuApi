b0c42f65812d727ecd5d120aeae6a1eae5a59e13
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 17:39:49 +0200

    delete not use code

--- src/ProcessManager/ProcessProvider.cpp
+++ src/ProcessManager/ProcessProvider.cpp
@@ -21,25 +21,6 @@ std::optional<ProcessInfo> ProcessProvider::FindProcessByName(const std::wstring
 	throw std::runtime_error("ProvideProcesses::FindProcessByName: No find process by name " + std::string(name.begin(), name.end()));
 }
 
-/*std::vector<ProcessInfo> ProcessProvider::GetAllProcesses() {
-	std::vector<ProcessInfo> processes;
-	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (snapshot == INVALID_HANDLE_VALUE) {
-		throw std::runtime_error("ProvideProcesses::GetAllProcesses: Failed to create process snapshot.");
-	}
-
-	PROCESSENTRY32W pe = { sizeof(pe) };
-	if (Process32FirstW(snapshot, &pe)) {
-		do {
-			ProcessInfo p{ pe.th32ProcessID, std::wstring(pe.szExeFile) };
-			processes.emplace_back(p);
-		} while (Process32NextW(snapshot, &pe));
-	}
-
-	CloseHandle(snapshot);
-	return processes;
-}*/
-
 std::vector<ProcessInfo> ProcessProvider::GetAllProcesses() {
 	std::vector<ProcessInfo> processes;
 	

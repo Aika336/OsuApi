3941188bb6838082f7416b2d12fb99a0b051a264
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 16:20:49 +0200

    fix: Fixes to the LoadModule function

--- NtDll/include/domain.h
+++ NtDll/include/domain.h
@@ -23,8 +23,23 @@ namespace nt_functions_name {
     static constexpr std::string_view QUERY_MEMORY = "NtQueryVirtualMemory";
 }
 
-HMODULE LoadModule(std::string_view module_name);
 std::wstring UnicodeStringToWString(const UNICODE_STRING& uStr);
+
+inline HMODULE LoadModule(std::string_view module_name)
+{
+    if (module_name.empty()) {
+        LOG("LoadModule: empty string module name");
+        return 0;
+    }
+
+    HMODULE hModule = GetModuleHandleA(module_name.data());
+    if (!hModule) {
+        LOG_ERROR("Failed to load module: " + std::string(module_name));
+        throw std::runtime_error("Failed to load module: " + std::string(module_name));
+    }
+
+    return hModule;
+}
 
 template<typename T>
 T LoadFunctionFromModule(HMODULE hModuel, std::string_view function_name) {
--- NtDll/src/domain.cpp
+++ NtDll/src/domain.cpp
@@ -1,21 +1,5 @@
 #include "domain.h"
 
-inline HMODULE LoadModule(std::string_view module_name)
-{
-    if (module_name.empty()) {
-        LOG("LoadModule: empty string module name");
-        return 0;
-    }
-
-    HMODULE hModule = GetModuleHandleA(module_name.data());
-    if (!hModule) {
-        LOG_ERROR("Failed to load module: " + std::string(module_name));
-        throw std::runtime_error("Failed to load module: " + std::string(module_name));
-    }
-
-    return hModule;
-}
-
 inline std::wstring UnicodeStringToWString(const UNICODE_STRING& uStr)
 {
     if (uStr.Buffer == nullptr || uStr.Length == 0) {

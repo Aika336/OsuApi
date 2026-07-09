34b1f555e2c10fa935f3cabc61757d346561d79f
Aika336 <vladock1999Suka@gmail.com>
2026-07-09 14:00:29 +0200

    feat: adding the main implementation to the domain for future use in wrappers for ntdll.dll

--- NtDll/include/domain.h
+++ NtDll/include/domain.h
@@ -1,6 +1,14 @@
 #pragma once
 
+#include "Logger.h"
+
+#include <Windows.h>
+#include <winternl.h>
+
 #include <string_view>
+#include <minwindef.h>
+#include <string>
+#include <bcrypt.h>
 
 namespace name_dll {
     static constexpr std::string_view NTDLL = "ntdll.dll";
@@ -12,4 +20,18 @@ namespace name_dll {
     static constexpr std::string_view BSOD = "NtRaiseHardError";
     static constexpr std::string_view READ_MEMORY = "NtReadVirtualMemory";
     static constexpr std::string_view QUERY_MEMORY = "NtQueryVirtualMemory";
+}
+
+HMODULE LoadModule(std::string_view module_name);
+std::wstring UnicodeStringToWString(const UNICODE_STRING& uStr);
+
+template<typename T>
+T LoadFunctionFromModule(HMODULE hModuel, std::string_view function_name) {
+    T func = reinterpret_cast<T>(GetProcAddress(hModuel, function_name));
+    if (!func) {
+        LOG("Incorrect load function: " + std::string(function_name));
+        throw std::runtime_error("Incorrect load function: " + std::string(function_name));
+    }
+
+    return func;
 }
